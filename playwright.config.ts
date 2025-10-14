e18710aa283b1602663428006c77b2bf403cf32a
steveholbrook <41897146+steveholbrook@users.noreply.github.com>
2025-10-15 09:29:23 +1100

    Add files via upload

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,2 @@
 import { defineConfig } from '@playwright/test';
-export default defineConfig({
-  testDir: './tests/e2e',
-  webServer:{ command:'npx http-server -p 8080 -c-1 .', port:8080, reuseExistingServer:!process.env.CI },
-  use:{ headless:true, baseURL:'http://localhost:8080' }
-});
+export default defineConfig({ testDir:'./tests/e2e', webServer:{ command:'npx http-server -p 8080 -c-1 .', port:8080, reuseExistingServer:!process.env.CI }, use:{ headless:true, baseURL:'http://localhost:8080' } });
