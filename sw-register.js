fcb9b4c87d6a4fe33d95be78a714430136788fa7
steveholbrook <41897146+steveholbrook@users.noreply.github.com>
2025-10-15 08:57:25 +1100

    Add files via upload

--- sw-register.js
+++ sw-register.js
@@ -1,8 +1,1 @@
-// sw-register.js
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.updated.js')
-      .then(reg => console.log('SW registered', reg.scope))
-      .catch(err => console.warn('SW registration failed', err));
-  });
-}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./service-worker.updated.js'))}
